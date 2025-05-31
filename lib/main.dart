b8f94d0e006bf6597712a6bb3b5e1a61c7f88ed4
maissa'Maissa-cloud-eng <hmaissa899@gmail.com>
2025-05-31 10:32:27 +0100

    country screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:training_dz/screens/api_screen.dart';
 import 'package:training_dz/screens/home_screen.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class CakeApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Cake App',
       theme: ThemeData(fontFamily: 'Roboto'),
-      home: const HomeScreen(),
+      home: const ApiFetchScreen(),
     );
   }
 }
--- lib/screens/api_screen.dart
+++ lib/screens/api_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ApiFetchScreen extends StatefulWidget {
   const ApiFetchScreen({super.key});
@@ -8,8 +11,57 @@ class ApiFetchScreen extends StatefulWidget {
 }
 
 class _ApiFetchScreen extends State<ApiFetchScreen> {
+  List<dynamic> countries = [];
+  bool isloading = true;
+  @override
+  void initState() {
+    super.initState();
+    fetchCountries();
+  }
+
+  Future<void> fetchCountries() async {
+    isloading = true;
+    final url = Uri.parse('https://restcountries.com/v3.1/all');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List data = json.decode(response.body);
+      setState(() {
+        countries = data;
+        isloading = false;
+      });
+    } else {
+      throw Exception('erreur de chargement');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: ElevatedButton(onPressed: () {}, child: Text("Fetch Data"))));
+    return Scaffold(
+      appBar: AppBar(title: Text('les pays du monde')),
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text("Fetch Data"),
+        onPressed: () {
+          fetchCountries();
+        },
+      ),
+      body:
+          isloading
+              ? Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                itemCount: countries.length,
+                itemBuilder: (context, index) {
+                  final country = countries[index];
+                  final name = country['name']?['common'] ?? 'inconnu';
+                  final flags = country['flags']?['png'] ?? '';
+                  return Card(
+                    child: ListTile(
+                      title: Text(name),
+                      leading: Image.network(flags, width: 60, height: 40),
+                      contentPadding: EdgeInsets.all(16),
+                    ),
+                  );
+                },
+              ),
+    );
   }
 }
